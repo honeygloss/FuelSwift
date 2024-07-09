07c1f6f79a270a7e321a6daa3fbf2beab0ff90ac
user <user@10.41.53.81>
2024-07-09 22:24:26 +0800

    push

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,37 +1,86 @@
-
 public class Customer {
-	 private int id;
-	    private String name;
-	    private String email;
+    private int id;
+    private String name;
+    private String email;
+    private String plateNo;
+    private String geranNo;
+    private String vehicleType;
+    private String password;
+    private String confirmPassword;
 
-	    public Customer(int id, String name, String email) {
-	        this.id = id;
-	        this.name = name;
-	        this.email = email;
-	    }
+    public Customer(int id, String name, String email, String plateNo, String geranNo, String vehicleType, String password, String confirmPassword) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.plateNo = plateNo;
+        this.geranNo = geranNo;
+        this.vehicleType = vehicleType;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
 
-	    // Getters and setters
-	    public int getId() {
-	        return id;
-	    }
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
 
-	    public void setId(int id) {
-	        this.id = id;
-	    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	    public String getName() {
-	        return name;
-	    }
+    public String getName() {
+        return name;
+    }
 
-	    public void setName(String name) {
-	        this.name = name;
-	    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	    public String getEmail() {
-	        return email;
-	    }
+    public String getEmail() {
+        return email;
+    }
 
-	    public void setEmail(String email) {
-	        this.email = email;
-	    }
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPlateNo() {
+        return plateNo;
+    }
+
+    public void setPlateNo(String plateNo) {
+        this.plateNo = plateNo;
+    }
+
+    public String getGeranNo() {
+        return geranNo;
+    }
+
+    public void setGeranNo(String geranNo) {
+        this.geranNo = geranNo;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
--- src/main/java/CustomerDAO.java
+++ src/main/java/CustomerDAO.java
@@ -1,42 +1,48 @@
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
 public class CustomerDAO {
-	 private String jdbcURL = "jdbc:mysql://localhost:3306/yourdatabase";
-	    private String jdbcUsername = "root";
-	    private String jdbcPassword = "password";
+    private String jdbcURL = "jdbc:mysql://localhost:3306/fuelSwiftDB";
+    private String jdbcUsername = "root";
+    private String jdbcPassword = "password";
 
-	    private static final String SELECT_ALL_CUSTOMERS = "SELECT * FROM customers";
+    private static final String SELECT_ALL_CUSTOMERS = "SELECT * FROM customers";
 
-	    public CustomerDAO() {}
+    public CustomerDAO() {}
 
-	    protected Connection getConnection() {
-	        Connection connection = null;
-	        try {
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        } catch (ClassNotFoundException e) {
-	            e.printStackTrace();
-	        }
-	        return connection;
-	    }
+    protected Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 
-	    public List<Customer> selectAllCustomers() {
-	        List<Customer> customers = new ArrayList<>();
-	        try (Connection connection = getConnection();
-	             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_CUSTOMERS);) {
-	            ResultSet rs = preparedStatement.executeQuery();
-	            while (rs.next()) {
-	                int id = rs.getInt("id");
-	                String name = rs.getString("name");
-	                String email = rs.getString("email");
-	                customers.add(new Customer(id, name, email));
-	            }
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        }
-	        return customers;
-	    }
-	}
+    public List<Customer> selectAllCustomers() {
+        List<Customer> customers = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_CUSTOMERS);) {
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String plateNo = rs.getString("plateNo");
+                String geranNo = rs.getString("geranNo");
+                String vehicleType = rs.getString("vehicleType");
+                String password = rs.getString("password");
+                String confirmPassword = rs.getString("confirmPassword");
+                customers.add(new Customer(id, name, email, plateNo, geranNo, vehicleType, password, confirmPassword));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return customers;
+    }
+}
